c2879c46ed8b7df7e96c23531cb757b290ce7024
Manuel <flo21029@byui.edu>
2024-03-01 22:40:50 -0300

    fix: Solve cart storing issue

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,16 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  const localStorageName = "so-cart";
+  let cartProducts = getLocalStorage(localStorageName);
+  if (cartProducts === null) {
+    cartProducts = [];
+  }
+  cartProducts.push(product);
+  setLocalStorage(localStorageName, cartProducts);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
